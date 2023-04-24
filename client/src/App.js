a211a9b2004137be1ead2cf0b64ebc2fcfecabae
komal kumari <komalsah342@gmail.com>
2023-04-24 22:14:08 +0530

    layout designed
    Client app created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>Ecommerce app</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
