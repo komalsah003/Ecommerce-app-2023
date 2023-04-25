be6adcc014031c884f81396b02f9c54954e91933
komal kumari <komalsah342@gmail.com>
2023-04-25 15:31:46 +0530

    navigation design
    navbar completed

---
+++ client/src/Pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Contact</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer Component</h1>
+    <div className="footer">
+      <h5 className="text-center">All Right Reserved &copy; KomalSah</h5>
+      <p className="text-center mt-3">
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/Policy">Privacy Policy</Link>
+      </p>
     </div>
   );
 };
